002cbd8828dfd834f3463c60dc86fa90931687a7
shingo inoue <inoueshingo@INOUESHINGOnoMacBook-Pro.local>
2022-12-24 16:47:26 +0900

    “ログインログアウト機能実装”

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception  
+  include SessionsHelper
+  before_action :login_required
+
+end
+
+private
+
+def login_required
+  redirect_to new_session_path unless current_user
+end
+end
+
