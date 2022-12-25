ce6d7c52e307227c935071b139246f6739d3a883
shingo inoue <inoueshingo@INOUESHINGOnoMacBook-Pro.local>
2022-12-25 14:31:03 +0900

    “プログ編集画面完了”

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,20 +1,44 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def new
-  end
-
-  def new
-    # 追記する
     @blog = Blog.new  
   end
 
-  # 追記する
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
-
-    redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end    
   end
 
+  def show
+    @blog = Blog.find(params[:id])    
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])  
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit 
+    end         
+  end
+
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
